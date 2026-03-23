034f1102fbcad06bd6bb0efb8c5a6f96cf378935
Bipin Koirala <bipinkoirala2061@gmail.com>
2026-03-23 22:27:32 +0545

    Balanced Binary Tree

---
+++ BalancedBinaryTree/Main.java
@@ -0,0 +1,8 @@
+package BalancedBinaryTree;
+
+public class Main {
+  static void main() {
+    BalancedBinaryTree binaryTree = new BalancedBinaryTree();
+    System.out.println(binaryTree.isBalanced(new TreeNode()));
+  }
+}
