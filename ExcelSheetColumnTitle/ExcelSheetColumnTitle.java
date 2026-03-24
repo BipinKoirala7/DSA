98a49fd1ccae223d1adb974ea5dbda9795743d25
Bipin Koirala <bipinkoirala2061@gmail.com>
2026-03-24 21:31:54 +0545

    Excel Sheet Column Title

---
+++ ExcelSheetColumnTitle/ExcelSheetColumnTitle.java
@@ -0,0 +1,17 @@
+package ExcelSheetColumnTitle;
+
+public class ExcelSheetColumnTitle {
+  //  2147483647
+  public String convertToTitle(int columnNumber) {
+    StringBuilder sb = new StringBuilder();
+
+    while (columnNumber > 0) {
+      columnNumber--;
+      int remainder = columnNumber % 26;
+      sb.append((char) ('A' + remainder));
+      columnNumber /= 26;
+    }
+
+    return sb.reverse().toString();
+  }
+}
---
+++ ExcelSheetColumnTitle/Main.java
@@ -0,0 +1,8 @@
+package ExcelSheetColumnTitle;
+
+public class Main {
+  static void main() {
+    ExcelSheetColumnTitle columnTitle = new ExcelSheetColumnTitle();
+    System.out.println("String: " + columnTitle.convertToTitle(2147483647));
+  }
+}
